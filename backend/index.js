ad04f445423c6182a48545074eb2b65b1f09afef
chobtham-thammakrang <t.chobtham2002@gmail.com>
2024-10-10 20:18:11 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,12 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-// Add this middleware
+// Update this middleware
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
