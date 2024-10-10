19d6db8b74a5bcdc89e7679cb46cf0382505f18c
chobtham-thammakrang <t.chobtham2002@gmail.com>
2024-10-10 20:05:58 +0700

    Update userLogout.js

--- backend/controller/user/userLogout.js
+++ backend/controller/user/userLogout.js
@@ -3,7 +3,7 @@ async function userLogout(req, res){
         const tokenOptions = {
             httpOnly: true,
             secure: true,
-            sameSite: 'none'
+            sameSite: 'lax'
         }
         res.clearCookie("token", tokenOptions);
 
