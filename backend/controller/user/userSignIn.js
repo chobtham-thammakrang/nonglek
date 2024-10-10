a8a56c22be3fa1149864cd893868c312025c16cb
chobtham-thammakrang <t.chobtham2002@gmail.com>
2024-10-10 20:35:33 +0700

    Update userSignIn.js

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -32,7 +32,7 @@ async function userSignInController(req, res){
             const tokenOptions = {
                 httpOnly: true,
                 secure: true,
-                sameSite: 'strict'
+                sameSite: 'none'
             }
             res.cookie('token', token, tokenOptions).status(200).json({
                 message: 'Login Success',
